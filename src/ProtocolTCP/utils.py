e68e149f4ba4f377e99ca5f7c2051b0a8d6b982e
Benjamin <benjamin.lecesne.blc@gmail.com>
2022-01-20 23:02:46 +0100

    feat: ajout de la fonction parse_command qui permet de parser correctement une commande clavier

--- src/ProtocolTCP/utils.py
+++ src/ProtocolTCP/utils.py
@@ -29,12 +29,29 @@ def get_path(base, go_to):
     if base == '.':
         base = os.getcwd()
     path = re.sub("/+", '/', go_to if go_to != "" and go_to[0] == '/' else base + '/' + go_to)
-    path_directories = [""]
+    path_directories = []
     for directory in path.split('/'):
         if directory == '..':
-            path_directories.pop()
-            if len(path_directories) <= 0:
-                path_directories.append("")
+            if len(path_directories) > 0:
+                path_directories.pop()
         elif directory != '.':
             path_directories.append(directory)
     return "/".join(path_directories)
+
+
+def parse_command(command):
+    args = command.strip().split(' ')
+    result = []
+    temp = []
+    for elem in args:
+        if elem[-1] == "\\" and elem[-2] != "\\":
+            temp.append(elem[:-1])
+        else:
+            temp.append(elem)
+            result.append(" ".join(temp).replace("\\\\", "\\"))
+            temp = []
+    if len(temp) != 0:
+        result.append(" ".join(temp).replace("\\\\", "\\"))
+    return result
+
+print(os.getcwd())
