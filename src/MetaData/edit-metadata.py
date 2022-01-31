80d024aa01a1bb999300fa0cf729b659eb7b97c1
Antoine <>
2022-01-31 15:18:10 +0100

    continue: Ajout de la modification des métadonnées d'un fichier pdf

--- src/MetaData/edit-metadata.py
+++ src/MetaData/edit-metadata.py
@@ -1,49 +1,24 @@
-import os
-
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-from sectime import setctime
 
-'''
-class PdfMeta(str):
-
-    def writeToStream(writer, stream, encryption_key):
-        stream.write(b_(writer))
-
-
-def test(writer, infos):
-    args = {}
-    print(infos.items())
-    for key, value in list(infos.items()):
-        args[PdfMeta(key)] = createStringObject(value)
-    print(args)
-    obj = writer.getObject(writer._info)
-    print(type(obj))
-    print(" : ")
-    print(obj)
-    writer.getObject(writer._info).update(args)
-'''
-
-fin = open('../../test/test_metadata/good.pdf', 'rb')
-reader = PdfFileReader(fin)
-
-writer = PdfFileWriter()
-
-writer.appendPagesFromReader(reader)
-metadata = reader.getDocumentInfo()
-writer.addMetadata(metadata)
-
-writer.addMetadata({
-    "/ModDate": "D:20000225143743+01'00'",
-    "/CreationDate": "D:20000225143743+01'00'"
-})
+def set_pdf_metadata(path, metadata):
+    file = open(path, 'rb')
+    reader = PdfFileReader(file)
+    writer = PdfFileWriter()
+    writer.appendPagesFromReader(reader)
+    file.close()
+    writer.addMetadata(metadata)
+    fout = open(f"{path}h", 'wb')
+    writer.write(fout)
+    fout.close()
 
 
-fout = open('2_edited.pdf', 'wb')
-writer.write(fout)
+def get_pdf_metadata(path):
+    file = open(path, 'rb')
+    reader = PdfFileReader(file)
+    metadata = reader.getDocumentInfo()
+    file.close()
+    return metadata
 
-fin.close()
-fout.close()
 
-
-os.utime("2_edited.pdf", (279990276, 279990276))
+set_pdf_metadata("../t.pdf", get_pdf_metadata("../r.pdf"))
