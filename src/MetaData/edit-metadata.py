a2cf47da77ae4ff5de1068bc009403ab13fd5ede
belecesne <you@gmail.com>
2022-02-09 15:13:37 +0100

    continue

--- src/MetaData/edit-metadata.py
+++ src/MetaData/edit-metadata.py
@@ -1,31 +1,40 @@
-from PyPDF2 import PdfFileReader, PdfFileMerger
+
 from sectime import setctime
 from timestamp import to_timestamp
 import os
+# from PyPDF2 import PdfFileReader, PdfFileWriter
+#
+#
+# def set_pdf_metadata(path_src, path_dst):
+#     reader = PdfFileReader(path_dst)
+#     writer = PdfFileWriter()
+#     writer.appendPagesFromReader(reader)
+#     writer.addMetadata(get_pdf_metadata(path_src))
+#
+#     file = open(path_src, 'rb')
+#
+#     fout = open(f"{path_dst}", 'wb')
+#     writer.write(fout)
+#     file.close()
+#     fout.close()
+#     try:
+#         setctime(f"{path_dst}", to_timestamp("23/03/1998 12:12:48"))
+#     except Exception as e:
+#         print(e)
+#     os.utime(f"{path_dst}", (os.path.getctime(path_src), os.path.getmtime(path_src)))
+#
+#
+# def get_pdf_metadata(path):
+#     file = open(path, 'rb')
+#     reader = PdfFileReader(file)
+#     metadata = reader.getDocumentInfo()
+#     file.close()
+#     return metadata
 
-
-def set_pdf_metadata(path_src, path_dst):
-    file = open(path_src, 'rb')
-    merger = PdfFileMerger()
-    merger.append(file)
-    fout = open(f"{path_dst}", 'wb')
-    merger.write(fout)
-    file.close()
-    fout.close()
-    try:
-        setctime(f"{path_dst}", to_timestamp("23/03/1998 12:12:48"))
-    except Exception as e:
-        print(e)
-    os.utime(f"{path_dst}", (os.path.getctime(path_src), os.path.getmtime(path_src)))
-
-
-def get_pdf_metadata(path):
-    file = open(path, 'rb')
-    reader = PdfFileReader(file)
-    metadata = reader.getDocumentInfo()
-    file.close()
-    return metadata
+def windows(path_dst):
+    setctime(f"{path_dst}", to_timestamp("23/03/1998 12:12:48"))
 
 if __name__ == '__main__':
-    set_pdf_metadata("../r.pdf", "../g.pdf")
+    windows("../j.pdf")
+    # set_pdf_metadata("../r.pdf", "../j.pdf")
 
